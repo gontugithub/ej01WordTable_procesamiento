772b5294ba69045db89f775cab00d8441d33bbf8
gontugithub <gs.alvarez8@gmail.com>
2024-10-08 12:43:23 +0200

    basic folder structure

---
+++ src/main/java/com/example/ej01WordTable/Ej01WordTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.ej01WordTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej01WordTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej01WordTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ej01WordTable/Ej01WordTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ej01WordTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej01WordTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
